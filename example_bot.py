97e0c3f631ac9cc94306a05ba071ee742e0252cb
linustrain <edenseb879@gmail.com>
2023-10-22 19:53:47 -0400

    Added total stats embed

--- example_bot.py
+++ example_bot.py
@@ -4,6 +4,7 @@ import discord
 import os
 import requests
 
+
 steam_id = {}
 
 intents = discord.Intents.default()
@@ -37,15 +38,45 @@ async def on_message(message):
             return
         stats_request = requests.get(url = 'https://royale.pet/api/player/' + steam_id[message.author.name] + '/stats')
         summary_request = requests.get(url = 'https://royale.pet/api/player/' + steam_id[message.author.name] + '/summary')
-        data = stats_request.json()
+        stats_data = stats_request.json()
         summary_data = summary_request.json()
+        print(stats_data)
         print(summary_data)
-        stats = data['stats']
+        stats = stats_data['stats']
+        solos_stats = {
+            'Games': stats['Games']['value'],
+            'Kills': stats['Kills']['value'], 
+            'MostKills': stats['MostKills']['value'],
+            'Top': stats['Top5']['value'],
+            'Deaths': stats['Deaths']['value'],
+            'Wins': stats['Wins']['value']
+        }
+        duos_stats = {
+            'Games': stats['GamesDuos']['value'],
+            'Kills': stats['KillsDuos']['value'], 
+            'MostKills': stats['MostKillsDuos']['value'],
+            'Top': stats['Top3Duos']['value'],
+            'Deaths': stats['DeathsDuos']['value'],
+            'Wins': stats['WinsDuos']['value']
+        }
+        squads_stats = {
+            'Games': stats['GamesSquads']['value'],
+            'Kills': stats['KillsSquads']['value'], 
+            'MostKills': stats['MostKillsSquads']['value'],
+            'Top': stats['Top2Squads']['value'],
+            'Deaths': stats['DeathsSquads']['value'],
+            'Wins': stats['WinsSquads']['value']
+        }
+        kills_total = solos_stats['Kills'] + duos_stats['Kills'] + squads_stats['Kills']
+        deaths_total = solos_stats['Deaths'] + duos_stats['Deaths'] + squads_stats['Deaths']
+        games_total = solos_stats['Games'] + duos_stats['Games'] + squads_stats['Games']
+        time_played = str(int(stats['TimePlayedSeconds']['value'] / 3600)) + " hours"
+        print(time_played)
         embed = discord.Embed(title='Player Stats for ' + summary_data['summary']['personaname'], colour = discord.Colour.blue())
-        embed.add_field(name='Kills', value=stats['Kills']['value'], inline=False)
-        embed.add_field(name='Deaths', value=stats['Deaths']['value'], inline=False)
-        embed.add_field(name='Games', value=stats['Games']['value'], inline=False)
-        embed.add_field(name='Time Played', value=stats['TimePlayedSeconds']['value'], inline=False)
+        embed.add_field(name='Kills', value=kills_total, inline=False)
+        embed.add_field(name='Deaths', value=deaths_total, inline=False)
+        embed.add_field(name='Games', value=games_total, inline=False)
+        embed.add_field(name='Time Played (In-match)', value=time_played, inline=False)
         embed.add_field(name='Health Juice Drank', value=stats['HealthJuiceDrank']['value'], inline=False)
         embed.add_field(name='Damage Dealt', value=stats['DamageDealt']['value'], inline=False)
 
